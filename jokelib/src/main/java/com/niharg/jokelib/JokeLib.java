94cf8975279b395c667553799947e3c6e5a0bd32
Nihar Gajare <nihargajre@gmail.com>
2016-05-04 21:19:27 +0530

    jokelib: Return a random joke from and array of jokes

--- jokelib/src/main/java/com/niharg/jokelib/JokeLib.java
+++ jokelib/src/main/java/com/niharg/jokelib/JokeLib.java
@@ -1,9 +1,18 @@
 package com.niharg.jokelib;
 
+import java.util.Calendar;
+import java.util.Random;
+
 public class JokeLib {
 
+    private static String[] jokes = {"Q: Why did the scarecrow win the Nobel Prize? \nA: He was out standing in his field.",
+    "An elephant walks into a pub and says \"Large scotch please barman.\"\n" +
+            "\"You again\" says the bar man \"Why do you drink so much?\"\n" +
+            "The elephant replies \"I do it to forget.\"" };
+
     public static String getJoke() {
-        return "Q: Why did the scarecrow win the Nobel Prize? \nA: He was out standing in his field.";
+        Random random = new Random(Calendar.getInstance().getTimeInMillis());
+        return jokes[random.nextInt(2)];
     }
 
 }
